362c6c6e73f7f47ef72c746cbeed2a35a54990cb
pranav K. <93134888+Pra-nav72@users.noreply.github.com>
2025-10-05 13:30:47 +0530

    Delete DYP-python directory
    upload delete. will add through terminal

--- DYP-python/DYP/O12Module_1.py
+++
@@ -1,4 +0,0 @@
-import O11Modules as m
-import O10functions as func
-print(m.add(2,5))
-print(func.add(1, 3, 4,5, 6))
--- DYP-python/imp.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print(sys.argv)
-print("print length of list :", len(sys.argv))
-print("0th index number: ", sys.argv[0])
